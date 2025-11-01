8e4edd775ab2b141c27f363a708f23e10489a3ab
DipankarSethi3012 <dipankarsethi3012@gmail.com>
2025-11-01 21:19:31 +0530

    (feat): Woks Using the OPen Source Model locaaly

---
+++ ChatModels/huggingface_local.py
@@ -0,0 +1,23 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+#HuggingFacePipeline is used when we are using local models that we download from huggging face and run locally
+import os
+#It will download model from Hugging Face and run locally using transformers library
+#Use only when we have gpus available otherwise it will be very slow on cpus
+
+#By deafult c drive-> you can set it to any directory where you have enough space to store models   
+os.environ['F_HOME'] = "D:/huggingface"  # Setting huggingface cache directory
+llm = HuggingFacePipeline(
+    model_id="MiniMaxAI/MiniMax-M2",
+    task="text-generation",
+    pipeline_kwargs=dict(
+        temperature=0.7,
+        max_new_tokens=100,
+    )
+
+)
+model = ChatHuggingFace(
+    llm = llm
+)
+
+result = model.invoke("What is the capital of India?")
+print(result.content)
